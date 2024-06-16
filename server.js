ea4ebd679d645c422ad9635f919fcf8738cd81b5
jakyungjung <newmade01@naver.com>
2024-06-16 16:40:16 +0900

    fs추가

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require("http");
 var count = 0;
+
+var fs = require("fs");
+
 http
   .createServer(function (request, response) {
-    count = count + 1;
+    fs.readFile("count.txt", function (err, data) {
+      if (data) count = parseInt(data);
+      count = count + 1;
+      fs.writeFileSync("count.txt", count.toString());
+    });
+
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end(count.toString());
   })
